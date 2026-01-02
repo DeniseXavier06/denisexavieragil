40976f3afb5319f90774e32926f9841e57be401f
Denise Xavier dos Santos <denise.santos@firstbps.com.br>
2026-01-02 16:59:15 -0300

    pagina

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+//import { componentTagger } from "lovable-tagger";
+
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig(({ command }) => ({
+ 
+  plugins: [react()],
+  base: command === "build" ? "/denisexavieragil/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "::",
+    port: 8080,
   },
 }));
